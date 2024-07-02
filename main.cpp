4ebe16638e270a4f3b613936b153591f0684b362
zach <zacharymd14@gmail.com>
2024-07-03 00:58:50 +0200

    initial

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+namespace fs = std::filesystem;
+
+int main(int argc, char *argv[]) {
+  cout << "Hello World !";
+  return 0;
+}
+
+int cleanCpp() { return 0; }
