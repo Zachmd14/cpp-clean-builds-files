d08fc35823af05fd1171b47de9b51bf17890ad0e
zach <zacharymd14@gmail.com>
2024-10-13 17:56:21 +0200

    cleaning

--- main.cpp
+++ main.cpp
@@ -1,11 +1,9 @@
+#include <filesystem>
 #include <iostream>
 
 using namespace std;
 namespace fs = std::filesystem;
 
-int main(int argc, char *argv[]) {
-  cout << "Hello World !";
-  return 0;
-}
+int main(int argc, char *argv[]) { return 0; }
 
 int cleanCpp() { return 0; }
